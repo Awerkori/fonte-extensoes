ed66dcc5664028bde30b0118f296f9af6ff176fe
Awerkori <awerkori@gmail.com>
2026-08-15 20:29:56 -0300

    Mangas Brasuka: v1.4.56 - Optimize reader performance and automate gate flow

--- src/pt/mangasbrasuka/build.gradle.kts
+++ src/pt/mangasbrasuka/build.gradle.kts
@@ -6,13 +6,16 @@ plugins {
 
 keiyoushi {
     name = "Mangas Brasuka"
-    versionCode = 3
+    versionCode = 56
     contentWarning = ContentWarning.MIXED
-    libVersion = "1.4"
-    theme = "madara"
+    libVersion = "1.6"
 
     source {
         lang = "pt-BR"
         baseUrl = "https://mangasbrasuka.com.br"
     }
+}
+
+dependencies {
+    implementation(project(":lib:cookieinterceptor"))
 }
