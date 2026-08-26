784854e1229f5188554e54caf07686876d0c3bc4
Awerkori <awerkori@gmail.com>
2026-08-26 02:32:25 -0300

    Publish Little Tyrant 1.4.64

--- src/pt/littletyrant/build.gradle.kts
+++ src/pt/littletyrant/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 keiyoushi {
     name = "Little Tyrant"
-    versionCode = 64
+    versionCode = 12
     contentWarning = ContentWarning.MIXED
     libVersion = "1.4"
     theme = "madaralegacy"
